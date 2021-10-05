eac0967467beb137226b02babdf200e81b86dc0a
Shahid0120 <shahid.hussain0120@gmail.com>
2021-10-05 20:22:36 +1100

    refractor the code base into yarn instead of npm cause npm is causing problem in the depency tree

---
+++ eccomerceindexeddb/src/components/nav/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <React.Fragment>
+      <div id="logo"></div>
+      <div id="nav-items">
+        <h1>Hi</h1>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Nav;

caed43042f192a17ecf7755705466e595c73fcc0
Shahid0120 <shahid.hussain0120@gmail.com>
2021-10-10 16:32:56 +1100

    make basic axios request and place slider data into array to display onto screen

--- src/components/homepage/assortedProducts/AssortedProducts.js
+++ src/components/homepage/assortedProducts/AssortedProducts.js
@@ -1,5 +1,39 @@
 import React from "react";
-import './AssortedProducts.css'
+import "./AssortedProducts.css";
+import axios from "axios";
+// make setup a api request using axios
+// print 4 images on this
+// length is 20
+// make 4 randon choices from the get request
+axios.get("https://fakestoreapi.com/products").then((res) => {
+  // this is a object of arrays of 20
+  let reqData = res.data;
+  let ImageSliderImages = [];
+  let randomNumber = 0;
+  let ImageSliderChoice = [];
+  let data = " ";
+  console.log(reqData);
+  console.log(reqData[0].image);
+  // Psudo Code
+  // goal: get 4 random objects from reqData and display that
+  // find length of reqData
+  // geenrat rnadom index number based on lenght
+  // then take objects from reqData and place into new array
+  // display that array
+  let x = 0;
+  while (x < 4) {
+    // find length of reqData
+    // randomnumber length between reqData.length and 1
+    randomNumber = Math.random() * (10 - 1) + 1;
+    console.log(randomNumber);
+    // randomNumber = 2;
+    // data = reqData[randomNumber].image;
+    // // -1 based on index
+    // ImageSliderImages.push(data);
+    // x++;
+  }
+  // console.log(ImageSliderImages);
+});
 function AssortedProducts() {
   return (
     <div id="assorted-images--container">

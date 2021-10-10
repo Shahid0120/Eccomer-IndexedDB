ece8c7fdd888570ec448cbe1cb66e1d705a1e9b0
Shahid0120 <shahid.hussain0120@gmail.com>
2021-10-10 20:32:20 +1100

    get all object into an array to display on screen

--- src/components/homepage/assortedProducts/AssortedProducts.js
+++ src/components/homepage/assortedProducts/AssortedProducts.js
@@ -2,50 +2,80 @@ import React from "react";
 import "./AssortedProducts.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
-// make setup a api request using axios
-// print 4 images on this
-// length is 20
-// make 4 randon choices from the get request
 
 function AssortedProducts() {
-  const [assortedImages, setAssortedImages] = useState(" ");
+  const [assortedProduct, setAssortedProduct] = useState([]);
 
   useEffect(() => {
     axios.get("https://fakestoreapi.com/products").then((res) => {
       // this is a object of arrays of 20
       let reqData = res.data;
       let randomNumber = 0;
-      let data = " ";
+      let image = " ";
+      let title = " ";
+      let price = " ";
+      let mapFourItems = [];
       let x = 0;
       while (x < 4) {
         // find length of reqData
         // randomnumber length between reqData.length and 1
         randomNumber = Math.floor(Math.random() * (20 - 1 + 1)) + 1;
         // -1 for index based
-        data = reqData[randomNumber - 1].image;
-        setAssortedImages(data);
+        image = reqData[randomNumber - 1].image;
+        price = reqData[randomNumber - 1].price;
+        title = reqData[randomNumber - 1].title;
+        mapFourItems.push([title, price, image]);
         x++;
       }
+      // console.log(mapFourItems);
+      setAssortedProduct(mapFourItems);
     });
+    // show one product
   }, []);
 
-  // const showAssortedImages = assortedImages.map((img) => {
+  // maps all the items
+  // const showProducts = assortedProduct.map((product) => {
+  //   return (
+  //     <>
+  //       <h1>{product[0]}</h1>
+  //       <img src={product[2]} />
+  //       <h2>{product[1]}</h2>
+  //     </>
+  //   );
+  // });
+
   //   return (
   //     <div>
-  //       <img href={img} />
+  //       {/* //   <h1>{product[0]}</h1>
+  //     //   <img src={product[2]} />
+  //     //   <h2>{product[1]}</h2> */}
+  //       {console.log(element)}
   //     </div>
   //   );
   // });
+
+  const rightArrowClick = () => {
+    console.log(assortedProduct["0"]["0"]);
+    let currentNumberArray = 0;
+    return (
+      <div>
+        <h1>{assortedProduct["currentNumberArray"]["0"]}</h1>
+        <img src={assortedProduct["currentNumberArray"]["2"]} />
+        <h2>{assortedProduct["currentNumberArray"]["1"]}</h2>
+      </div>
+    );
+  };
 
   return (
     <div id="assorted-images--container">
       <div className="container">
         <h1>hi</h1>
-        {console.log(assortedImages)}
-        <img src={assortedImages} />
-        {/* {assortedImages.map((img) => {
-          return <img href={img} />;
-        })} */}
+        {/* {console.log(assortedProduct[0]["0"])} */}
+        {rightArrowClick}
+        {/* <h1>{assortedProduct[0]["0"]}</h1>
+        <img src={assortedProduct[0]["2"]} />
+        <h2>{assortedProduct[0]["1"]}</h2> */}
+        <button onClick={rightArrowClick}>RIGHT</button>
       </div>
     </div>
   );

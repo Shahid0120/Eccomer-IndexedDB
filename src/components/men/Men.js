57502ca6d986c9d112223e6814765c9203bbe397
Shahid0120 <shahid.hussain0120@gmail.com>
2021-10-18 21:55:36 +1100

    make bussiness logic for filtering products

--- src/components/men/Men.js
+++ src/components/men/Men.js
@@ -2,7 +2,7 @@ import React from "react";
 import Nav from "../nav/Nav";
 import Footer from "../footer/Footer";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useReducer } from "react";
 /*
 Checklist 
 1. Api request (promise so its async) (done)
@@ -31,8 +31,6 @@ Checklist
 
   Current 
   Filter for clothing
-    ---render Buttons statically 
-    -- onClick => state Changes from default of filter 
     -- Change of State cause re-rednering of items 
     -- change based on change 
 */
@@ -40,7 +38,11 @@ Checklist
 function Men() {
   const [allProducts, setAllProducts] = useState([{}]);
   const [allMensProducts, setAllMensProducts] = useState([{}]);
+  const [individualProduct, setIndividualProduct] = useState({});
+  const [indexNumber, setIndexNumber] = useState(0);
   const [filters, setFilters] = useState("Random");
+  const [_, forceUpdate] = useReducer((x) => x + 1, 0);
+
   const filtersButton = [
     "Random",
     "Decending Price",
@@ -65,7 +67,8 @@ function Men() {
     }, []);
   }, []);
 
-  const rederAllMensProducts = allMensProducts.map((garment, index) => {
+  // if filters changes
+  const renderAllMensProducts = allMensProducts.map((garment, index) => {
     return (
       <>
         <div className={garment.title}>
@@ -77,27 +80,77 @@ function Men() {
     );
   });
 
-  // const filter = filtersButton.map((button) => {
-  //   return (
-  //     <>
-  //       <button className={button}>{button}</button>
-  //     </>
-  //   );
-  // });
-  const filterClick = (button) => {
-    console.log(button);
+  const filterClick = (event) => {
+    setFilters(event.target.innerText);
+    forceUpdate();
   };
+
+  const buttonFilterProducts = () => {
+    // take current filters state
+    // switch case
+    // price assending
+    // assorte products by price
+    // change state of allMensProducts
+    // price dessending
+    // assort product by decending price
+    // change state of allMensProducts
+    // alphbetical
+    //assort by alphabetical
+    // change state of allMensProducts
+
+    // how to change state of
+    let newCurrentMensArrangementBasedOnFilter = [];
+    let currentStateOfAllMensProducts = [...allMensProducts];
+    let currentFilter = filters;
+
+    switch (currentFilter) {
+      case "Acending Price":
+        //setAllMensProducts
+        // new newCurrentMensArrangementBasedOnFilter push all mens products in order of price assending into the array
+        while (
+          newCurrentMensArrangementBasedOnFilter <=
+          currentStateOfAllMensProducts.length
+        ) {
+          // find lowest price
+          let currentLowestPrice = 100000;
+          let indexCount = 0;
+          currentStateOfAllMensProducts.map((mensProducts, index) => {
+            // filer by price
+            // finds lowest
+            // find the lowest price
+            if (mensProducts.price <= currentLowestPrice) {
+              currentLowestPrice = mensProducts.price;
+              setIndividualProduct(mensProducts);
+              setIndexNumber(indexCount);
+            }
+            indexCount += 1;
+          });
+          newCurrentMensArrangementBasedOnFilter.push(individualProduct);
+          currentStateOfAllMensProducts.splice(indexNumber, 1);
+        }
+        setAllMensProducts(newCurrentMensArrangementBasedOnFilter);
+        break;
+      case "Decending Price":
+        break;
+      case "Alphabetically":
+        break;
+      default:
+        break;
+    }
+    return <>{renderAllMensProducts}</>;
+  };
+
   return (
     <div>
       <Nav />
       <h3>This is show the current page path </h3>
-      <h1>TiTLE</h1>
+      <h1>Title</h1>
       <div id="filers--container">
         <div id="filters--options">
           {filtersButton.map((button) => {
             return (
               <>
-                <button className={button} onChange={console.log(button)}>
+                <button className={button} onClick={filterClick}>
                   {button}
                 </button>
               </>
@@ -107,8 +160,7 @@ function Men() {
       </div>
       <h1>Products</h1>
       <h1>-----------</h1>
-      {rederAllMensProducts}
-
+      {filters === "Random" ? renderAllMensProducts : buttonFilterProducts}
       <Footer />
     </div>
   );
